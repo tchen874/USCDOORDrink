bb24d50e99179e550da789849af2ce20947b34ce
tiffanychen <tchen874@usc.edu>
2022-03-19 12:10:38 -0700

    ui

---
+++ app/src/main/java/com/example/uscdoordrink/Ｕser.java
@@ -0,0 +1,16 @@
+package com.example.uscdoordrink;
+
+public class Ｕser {
+    private String username;
+    private String password;
+
+
+    // On the ui
+    public String viewAccount()
+    {
+        return username;
+    }
+
+
+
+}
