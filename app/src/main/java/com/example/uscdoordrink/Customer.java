61756b9fa0954bed30994d170136191f7793e7ac
Madison Brading <brading@usc.edu>
2022-03-29 19:52:32 -0700

    //adding basic customer variables because will be using them and they are needed for basic functionality of app

--- app/src/main/java/com/example/uscdoordrink/Customer.java
+++ app/src/main/java/com/example/uscdoordrink/Customer.java
@@ -3,6 +3,19 @@ package com.example.uscdoordrink;
 import java.util.ArrayList;
 
 public class Customer {
+
+    //adding basic customer variables because will be using them and they are needed for basic functionality of app
+    public static final String drink_name_ = "drink_name";
+    public String id;
+    public String customer_name;
+    public String drink;
+    public String startTime;
+    public String endTime;
+    public String seller_name;
+    public String restaurant_name;
+    public Boolean caffeine;
+    public String order_location;
+
     private String custName;
     //private Location custLoc;
     private ArrayList<Order> currOrders;
