2f6a3fb2aa2565a8fda6d4eda35400a1fdc5325e
Madison Brading <brading@usc.edu>
2022-04-26 13:12:43 -0700

    added weather location feature + updated fragment view

--- app/src/main/java/com/example/uscdoordrink/frag1_overview.java
+++ app/src/main/java/com/example/uscdoordrink/frag1_overview.java
@@ -1,11 +1,16 @@
 package com.example.uscdoordrink;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.SyncNotedAppOp;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
@@ -17,6 +22,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,7 +34,10 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -36,6 +45,7 @@ import org.json.JSONObject;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.concurrent.Executor;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -49,8 +59,13 @@ public class frag1_overview extends Fragment {
     }
 
     String weather_url = "";
-    String api_key ="6e885470367f4e05810cfd5a93c995d0";
+    String api_key = "6e885470367f4e05810cfd5a93c995d0";
     TextView textView;
+    private boolean locationPermissionGranted;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private Location lastKnownLocation;
+    private final LatLng defaultLocation = new LatLng(34.0224, 118.2851);
+
 
     private FusedLocationProviderClient fusedLocationClient;
 
@@ -76,43 +91,55 @@ public class frag1_overview extends Fragment {
         View v = inflater.inflate(R.layout.fragment_frag1_overview, container, false);
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
         textView = v.findViewById(R.id.textView);
-
+        //getLocationPermission();
 
         Log.e("lat", weather_url);
         // on clicking this button function to
         // get the coordinates will be called
         Button btVar1 = (Button) v.findViewById(R.id.btVar1);
 
-        btVar1.setOnClickListener(v1 -> {
-            System.out.println("afterSetOnClickListener " + fusedLocationClient);
-            Log.e("lat", "onClick");
-            // function to find the coordinates
-            // of the last location
-            obtainLocation();
+        Log.e("print anything", "testing");
+        //LatLng origin = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+
+
+        btVar1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                System.out.println("afterSetOnClickListener " + fusedLocationClient);
+                Log.e("lat", "onClick");
+                obtainLocation();
+                // of the last location
+                // and create the http URL
+                //weather_url = "https://api.weatherbit.io/v2.0/current?" + "lat=" + location.getL + "&lon=" + defaultLocation.longitude + "&key=" + api_key;
+                //getTemp();
+            }
         });
-        return inflater.inflate(R.layout.fragment_frag1_overview, container, false);
+        return v;
+        //return inflater.inflate(R.layout.fragment_frag1_overview, container, false);
     }
 
 
     @SuppressLint("MissingPermission")
-    private void obtainLocation(){
+    private void obtainLocation() {
         Log.e("lat", "function");
         // get the last location
-
-        fusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                // and create the http URL
-                weather_url = "https://api.weatherbit.io/v2.0/current?" + "lat=" + location.getLatitude() + "&lon=" + location.getLongitude() + "&key=" + api_key;
-                Log.e("lat", weather_url);
-                System.out.println("in obtainLocation fusedLocClient " + fusedLocationClient);
-                System.out.println("log: "+ Log.e("lat", weather_url));
-                System.out.println("location=: "+ location.getLatitude());
-                // this function will
-                // fetch data from URL
-                getTemp();
-            }
-        });
+        getDeviceLocation();
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // and create the http URL
+                        weather_url = "https://api.weatherbit.io/v2.0/current?" + "lat=" + location.getLatitude() + "&lon=" + location.getLongitude() + "&key=" + api_key;
+                        Log.e("lat", weather_url);
+                        System.out.println("in obtainLocation fusedLocClient " + fusedLocationClient);
+                        System.out.println("log: " + Log.e("lat", weather_url));
+                        System.out.println("location=: " + location.getLatitude());
+                        // this function will
+                        // fetch data from URL
+                        getTemp();
+                    }
+                });
 
         }
 
@@ -120,7 +147,7 @@ public class frag1_overview extends Fragment {
             RequestQueue queue = Volley.newRequestQueue(getContext());
             String url = weather_url;
             Log.e("lat", url);
-            System.out.println("queue within getTemp() "+ queue );
+            System.out.println("queue within getTemp() " + queue);
             // Request a string response from the provided URL.
             StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                     new Response.Listener<String>() {
@@ -131,12 +158,31 @@ public class frag1_overview extends Fragment {
                             try {
                                 JSONObject obj = new JSONObject(response);
                                 JSONArray arr = obj.getJSONArray("data");
-                                System.out.println("JSONARRAY: "+ arr );
-                                System.out.println("JSONOBJ: "+ obj );
+                                System.out.println("JSONARRAY: " + arr);
+                                System.out.println("JSONOBJ: " + obj);
 
                                 JSONObject obj2 = arr.getJSONObject(0);
                                 Log.e("lat obj2", obj2.toString());
-                                textView.setText(obj2.getString("temp")+ " deg Celsius in " + obj2.getString("city_name"));
+                                double fahrenheit = Double.parseDouble(obj2.getString("temp"));
+                                fahrenheit = (fahrenheit *(1.8) + 32);
+                                String weather = " Since it is " + fahrenheit + " degrees fahrenheit in " + obj2.getString("city_name") + " where you're located,";
+                                //calculate what to recommend user to drink depending on weather
+                                int lowTemp = 46;
+                                int roomTemp = 72;
+                                int highTemp = 89;
+                                if (fahrenheit <= lowTemp) {
+                                    weather += " we recommend you drink a hot beverage today since it is cold outside! Warm up soon ";
+                                }
+                                if ((fahrenheit > lowTemp) &&  (fahrenheit < roomTemp)) {
+                                    weather += " we recommend you drink a warm beverage today since it is slightly below room temperature outside! ";
+                                }
+                                if ((fahrenheit >= roomTemp) &&  (fahrenheit < highTemp)) {
+                                    weather += " we recommend you drink a cold beverage today since it is above room temperature outside! ";
+                                }
+                                if ((fahrenheit >= highTemp) ) {
+                                    weather += " we recommend you drink a cold beverage IMMEDIATELY today since it is very hot outside! ";
+                                }
+                                textView.setText(weather);
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
@@ -147,10 +193,46 @@ public class frag1_overview extends Fragment {
                     System.out.print("That didn't work ");
                 }
             });
-            System.out.println("queue before: "+ queue );
+            System.out.println("queue before: " + queue);
             queue.add(stringRequest);
-            System.out.println("queue after: "+ queue );
+            System.out.println("queue after: " + queue);
 
+        }
+
+        private void getLocationPermission () {
+            /*
+             * Request location permission, so that we can get the location of the
+             * device. The result of the permission request is handled by a callback,
+             * onRequestPermissionsResult.
+             */
+            if (ContextCompat.checkSelfPermission(this.getContext(),
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                locationPermissionGranted = true;
+            } else {
+                ActivityCompat.requestPermissions(this.getActivity(),
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            }
+        }
+//
+        private void getDeviceLocation() {
+            try {
+                if (locationPermissionGranted) {
+                    //get current location
+                    Task<Location> locationResult = fusedLocationClient.getLastLocation();
+                    locationResult.addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) { //if not null, move camera
+                                lastKnownLocation = location;
+                            }
+                        }
+                    });
+                }
+            } catch (SecurityException e) {
+                Log.e("Exception: %s", e.getMessage(), e);
+            }
         }
 
     }
